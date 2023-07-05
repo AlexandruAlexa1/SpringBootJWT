9e9e46b7d6fff9e8afbc0fec083c982d5399e9e1
Alexandru Alexa <x.alx@yahoo.com>
2023-07-05 18:40:49 +0300

    Code TokenVerifier

--- src/main/java/com/aa/constant/SecurityConstant.java
+++ src/main/java/com/aa/constant/SecurityConstant.java
@@ -5,6 +5,8 @@ public class SecurityConstant {
 	public static final String AUTHORITIES = "authorities";
 	public static final long EXPIRATION_TIME = 604_800_000; // 7 Days
 	public static final String JWT_SECRET = "AA10^Ey$y7T^tYGqpe!U@8G*V*S5SmXyYC4zWSRx+9*HyR#)tvS#Xj#D#uzF)10G";
-	
+	public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+	public static final String TOKEN_REQUIRED = "Token is required";
+	public static final String SUBJECT_REQUIRED = "Subject is required";
 }
  
