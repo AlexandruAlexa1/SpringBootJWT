84d7597f81d29ca34632fae484065a62c03adb26
Alexandru Alexa <x.alx@yahoo.com>
2023-07-05 17:34:00 +0300

    Code RoleRepository

---
+++ src/main/java/com/aa/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.aa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aa.domain.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+}
