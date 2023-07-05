c4b5dbc8832110256548d883feebaa6d15f91c51
Alexandru Alexa <x.alx@yahoo.com>
2023-07-05 18:37:59 +0300

    Code TokenProvider

---
+++ src/main/java/com/aa/constant/SecurityConstant.java
@@ -0,0 +1,10 @@
+package com.aa.constant;
+
+public class SecurityConstant {
+
+	public static final String AUTHORITIES = "authorities";
+	public static final long EXPIRATION_TIME = 604_800_000; // 7 Days
+	public static final String JWT_SECRET = "AA10^Ey$y7T^tYGqpe!U@8G*V*S5SmXyYC4zWSRx+9*HyR#)tvS#Xj#D#uzF)10G";
+	
+}
+ 
